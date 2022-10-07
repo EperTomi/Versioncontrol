9405ef010531c16dd4581d05dfcbe329451b6ccd
haktzt <eada2222@gmail.com>
2022-10-07 12:20:47 +0200

    5. feladatigkesz

--- 4gyak/4gyak/Form1.cs
+++ 4gyak/4gyak/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace _4gyak
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
         }
     }
 }
