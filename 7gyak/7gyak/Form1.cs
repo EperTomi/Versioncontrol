2eb41482d3fde99014949349394c8045dd6ea74d
haktzt <eada2222@gmail.com>
2022-11-11 12:30:49 +0100

    4f

---
+++ 7gyak/7gyak/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7gyak
+{
+    public partial class Form1 : Form
+    {
+
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Tick.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+    }
+}
